fead5e39924a7a02e5df1cf2ef2f329ee129d120
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-05-12 08:36:49 +0200

    fix EventController.cs

--- hangnow-back/Controllers/EventController.cs
+++ hangnow-back/Controllers/EventController.cs
@@ -1,16 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using hangnow_back.Authentications;
 using hangnow_back.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace hangnow_back.Controllers;
 
-[ApiController]
 [Route("api/[controller]")]
+[ApiController]
 public class EventController : ControllerBase
 {
     private readonly Context _context;
+    private readonly UserManager<User> _userManager;
 
-    public EventController(Context context)
+    public EventController(Context context, UserManager<User> userManager)
     {
         _context = context;
+        _userManager = userManager;
+    }
+
+    // GET: api/event
+    [HttpGet]
+    public async Task<List<Event>> Get()
+    {
+        return await _context.Events.ToListAsync();
+    }
+
+    // GET: api/event/5
+    [HttpGet("{id:guid}")]
+    public async Task<Event?> Get(Guid id)
+    {
+        return await _context.Events.FirstOrDefaultAsync(e => e.Id == id);
+    }
+
+    // POST: api/event
+    [Authorize]
+    [HttpPost]
+    public async Task<Event> Post([FromBody] Event value)
+    {
+        value.OwnerId = HttpContext.User.GetId();
+        var newEvent = _context.Events.Add(value);
+        await _context.SaveChangesAsync();
+        return newEvent.Entity;
+    }
+
+    // PUT: api/Event/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE: api/Event/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
