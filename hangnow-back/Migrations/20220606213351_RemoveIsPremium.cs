6b0f1685a798568f0968e63fab4131df33153e9b
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-06-06 23:34:00 +0200

    remove ispremium field

---
+++ hangnow-back/Migrations/20220606213351_RemoveIsPremium.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hangnow_back.Migrations
+{
+    public partial class RemoveIsPremium : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPremium",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPremium",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
